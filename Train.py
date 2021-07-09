02622265f003e5326f8d886ed9f9506532ea6836
rushilkoppaka-freezonlabs <83945787+rushilkoppaka-freezonlabs@users.noreply.github.com>
2021-07-09 18:40:55 +0530

    Add files via upload

---
+++ Train.py
@@ -0,0 +1,26 @@
+import cv2
+# import numpy as np
+# import pytesseract
+# import NER
+# import Perspective_image
+import tesseract_OCR
+import bbox
+
+
+def Train(filename='Sample.jpeg',coordinates = 'yolo.txt',classes = 'classes.txt',dict={}):
+    img = cv2.imread(filename)
+    img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    print(img.shape)
+    class_list,location = bbox.get_bbox(img,coordinates,classes)     #getting bounding box and respective class
+    for i in range(0,len(location)):
+        dict[class_list[i]]= ''
+    for i in range(len(location)):
+        x,y,w,h = location[i]
+        print(class_list[i],' ',img[int(y):int(y + h), int(x):int(x + w)].shape, ' ', x, ' ', w,' ',y,' ',h)
+        image, line_conf,sentence= tesseract_OCR.img_data(img[int(y):int(y + h), int(x):int(x + w)])
+        dict[class_list[i]] = dict[class_list[i]] +sentence + '\n'
+    print(dict)
+    return dict
+
+
+
