d603de974f3654384519cc03e00439758762b03a
rushilkoppaka-freezonlabs <83945787+rushilkoppaka-freezonlabs@users.noreply.github.com>
2021-07-09 15:02:23 +0530

    Update bbox.py

--- bbox.py
+++ bbox.py
@@ -37,39 +37,5 @@ def get_bbox(image,coordinates,classes):
 
 
 
-if __name__ == '__main__':
-   coordinates = 'yolo.txt'
-   classes = 'classes.txt'
-   image = cv2.imread('Sample.jpeg')
-   bbox = []
-   dict_class = {}
-
-   c = open(classes, 'r')
-   cl = c.readlines()
-   for i in range(len(cl)):
-      dict_class[float(i)] = cl[i].strip()
-
-   f = open(coordinates, 'r')
-   data = f.readlines()
-   for i in range(len(data)):
-      data[i] = data[i].strip()
-      data[i] = data[i].split(' ')
-      for j in range(0, len(data[i])):
-         data[i][j] = float(data[i][j])
-      if dict_class[data[i][0]] != 'table':
-         _, x, y, w, h = data[i]
-         x = x * image.shape[0]
-         w = w * image.shape[0]
-         x = x - w / 2
-         h = h * image.shape[1]
-         y = y * image.shape[1]
-         y = y - h / 2
-         bbox.append(bbox, image[int(y):int(y + h), int(x):int(x + w)])
-
-   f.close()
-   c.close()
-
-
-
 
 
