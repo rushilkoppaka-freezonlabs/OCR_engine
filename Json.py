f2f10c849829f9f59c77ac17b940917d892e4702
rushilkoppaka-freezonlabs <83945787+rushilkoppaka-freezonlabs@users.noreply.github.com>
2021-07-24 15:08:23 +0530

    Update Json.py

--- Json.py
+++ Json.py
@@ -1,6 +1,5 @@
 import json
 import Train
-invoice_dict,tables = Train.Train('Sample.jpeg','yolo.txt')
+invoice_dict = Train.Train('Sample.jpeg','yolo.txt')
 with open('data.json', 'w') as fp:
     json.dump(invoice_dict, fp)
-print(tables)
